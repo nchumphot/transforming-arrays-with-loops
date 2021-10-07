392d6ed40126e051b54eed426b04dfc102758529
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-10-07 12:45:36 +0100

    Added a test case

---
+++ 0-lowercase-domains.js
@@ -0,0 +1,10 @@
+/* function: return lowercase of strings in an array
+=======================
+name: returnLowercase
+parameter NAMES: Array<string>
+return RESULT: Array<string>
+=======================
+*/
+
+// TEST CASES
+console.log('returnLowercase(["academy.tech", "GITHUB.COM", "StackOverflow.com", "mOzIlLa.OrG"]):',returnLowercase(["academy.tech", "GITHUB.COM", "StackOverflow.com", "mOzIlLa.OrG"]),'should return ["academy.tech", "github.com", "stackoverflow.com", "mozilla.org"]');
